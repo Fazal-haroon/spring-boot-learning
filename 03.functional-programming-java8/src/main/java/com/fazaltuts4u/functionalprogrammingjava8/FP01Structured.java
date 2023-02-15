5e494e16ab8191793e8d3779a4b4bbf99e75d1b4
Fazal Haroon <kharoon187@gmail.com>
2023-02-15 19:00:36 +0400

    Functional stream Approach to print

--- 03.functional-programming-java8/src/main/java/com/fazaltuts4u/functionalprogrammingjava8/FP01Structured.java
+++ 03.functional-programming-java8/src/main/java/com/fazaltuts4u/functionalprogrammingjava8/FP01Structured.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 public class FP01Structured {
     public static void main(String[] args) {
-        printAllNumbersInListStructured(List.of(21,2,3,4,11,25,15,112));
+//        printAllNumbersInListStructured(List.of(21,2,3,4,11,25,15,112));
+        printAllNumbersInListFunctional(List.of(21,2,3,4,11,25,15,112));
+    }
+
+    private static void printAllNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream().forEach(FP01Structured::print);
+    }
+
+    private static void print(int number){
+        System.out.println("number = " + number);
     }
 
     private static void printAllNumbersInListStructured(List<Integer> numbers) {
