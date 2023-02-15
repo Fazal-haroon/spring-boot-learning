fcc1f8b58f789e03f41a86d4030f6fc53d13fd5c
Fazal Haroon <36573782+Fazal-haroon@users.noreply.github.com>
2023-02-15 18:41:54 +0400

    functional-programming-java8 added

---
+++ 03.functional-programming-java8/src/main/java/com/fazaltuts4u/functionalprogrammingjava8/FunctionalProgrammingJava8Application.java
@@ -0,0 +1,13 @@
+package com.fazaltuts4u.functionalprogrammingjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalProgrammingJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalProgrammingJava8Application.class, args);
+	}
+
+}
---
+++ 03.functional-programming-java8/src/test/java/com/fazaltuts4u/functionalprogrammingjava8/FunctionalProgrammingJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazaltuts4u.functionalprogrammingjava8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalProgrammingJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
