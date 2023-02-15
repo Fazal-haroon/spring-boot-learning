6c416b2cb2007b52d3d0487f1518b31ca8168301
Fazal Haroon <kharoon187@gmail.com>
2023-02-15 19:20:55 +0400

    Improve Functional Programming with filter

--- 03.functional-programming-java8/src/main/java/com/fazaltuts4u/functionalprogrammingjava8/FP01Structured.java
+++ 03.functional-programming-java8/src/main/java/com/fazaltuts4u/functionalprogrammingjava8/FP01Structured.java
@@ -5,20 +5,35 @@ import java.util.List;
 public class FP01Structured {
     public static void main(String[] args) {
 //        printAllNumbersInListStructured(List.of(21,2,3,4,11,25,15,112));
-        printAllNumbersInListFunctional(List.of(21,2,3,4,11,25,15,112));
+//        printAllNumbersInListFunctional(List.of(21, 2, 3, 4, 11, 25, 15, 112));
+        printEvenNumbersInListStructured(List.of(21,2,3,4,11,25,15,112));
+        printEvenNumbersInListFunctional(List.of(21,2,3,4,11,25,15,112));
     }
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
-        numbers.stream().forEach(FP01Structured::print);
+//        numbers.stream().forEach(num -> System.out.println(num));
+        numbers.stream().forEach(System.out::println);
     }
 
-    private static void print(int number){
-        System.out.println("number = " + number);
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream().filter(num -> num%2==0).forEach(System.out::println);
     }
+
+//    private static void print(int number){
+//        System.out.println("number = " + number);
+//    }
 
     private static void printAllNumbersInListStructured(List<Integer> numbers) {
         for(int number: numbers){
             System.out.println("number = " + number);
+        }
+    }
+
+    private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                System.out.println("number = " + number);
+            }
         }
     }
 }
