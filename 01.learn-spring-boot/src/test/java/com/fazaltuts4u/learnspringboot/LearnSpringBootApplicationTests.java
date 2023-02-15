be91f22b9bc94fdcc8977cd7a8e999086e9a646d
Fazal Haroon <36573782+Fazal-haroon@users.noreply.github.com>
2023-02-15 11:31:52 +0400

    01.learn-spring-boot added

---
+++ 01.learn-spring-boot/src/test/java/com/fazaltuts4u/learnspringboot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazaltuts4u.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
